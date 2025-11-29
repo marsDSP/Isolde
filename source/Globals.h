77fb3068c0e580f2f3deecd1eb04da75bccaf869
Mars <210487034+marsDSP@users.noreply.github.com>
2025-11-28 20:17:00 -0500

    Added apvts & parameters

---
+++ source/Globals.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Includes.h>
+
+inline const juce::ParameterID speedParamID { "speed", 1 };
+static constexpr const char* speedParamIDName = "Speed";
+
+inline const juce::ParameterID randoParamID { "rando", 1 };
+static constexpr const char* randoParamIDName = "Rando";
+
+inline const juce::ParameterID depthParamID { "depth", 1 };
+static constexpr const char* depthParamIDName = "Depth";
+
+inline const juce::ParameterID regenParamID { "regen", 1 };
+static constexpr const char* regenParamIDName = "Regen";
+
+inline const juce::ParameterID derezParamID { "derez", 1 };
+static constexpr const char* derezParamIDName = "DeRez";
+
+inline const juce::ParameterID bufferParamID { "buffer", 1 };
+static constexpr const char* bufferParamIDName = "Buffer";
+
+inline const juce::ParameterID outputParamID { "output", 1 };
+static constexpr const char* outputParamIDName = "Output";
+
+inline const juce::ParameterID drywetParamID { "drywet", 1 };
+static constexpr const char* drywetParamIDName = "DryWet";
--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -2,22 +2,14 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-PluginProcessor::PluginProcessor()
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
+PluginProcessor::PluginProcessor() : AudioProcessor (BusesProperties()
                        .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
                        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+                       ), params(vts)
 {
 }
 
-PluginProcessor::~PluginProcessor()
-{
-}
-
+PluginProcessor::~PluginProcessor() = default;
 //==============================================================================
 const juce::String PluginProcessor::getName() const
 {
@@ -128,27 +120,22 @@ void PluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
 //==============================================================================
 bool PluginProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true;
 }
 
 juce::AudioProcessorEditor* PluginProcessor::createEditor()
 {
-    return new PluginEditor (*this);
+    return new juce::GenericAudioProcessorEditor (*this);
 }
 
 //==============================================================================
 void PluginProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
     juce::ignoreUnused (destData);
 }
 
 void PluginProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
     juce::ignoreUnused (data, sizeInBytes);
 }
 
